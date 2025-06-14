60a96a72fe29beb968d279c9161b8c013467731a
meckbaig <meckbaig@yandex.ru>
2025-06-14 16:03:05 +0500

    Add web dav media uploader service

---
+++ src/UltimateMessengerSuggestions/Services/IMediaUploader.cs
@@ -0,0 +1,25 @@
+namespace UltimateMessengerSuggestions.Services;
+
+/// <summary>
+/// Interface for uploading files to a WebDAV server.
+/// </summary>
+public interface IMediaUploader
+{
+	/// <summary>
+	/// Asynchronously uploads a file and returns the URL of the uploaded file.
+	/// </summary>
+	/// <remarks>The method uploads the provided file to a storage service and returns the URL where the file can be
+	/// accessed. Ensure that the file meets any size or format requirements imposed by the storage service.</remarks>
+	/// <param name="file">The file to be uploaded.</param>
+	/// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
+	/// <returns>URL of the uploaded file.</returns>
+	Task<string> UploadAsync(IFormFile file, CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Deletes the specified file asynchronously.
+	/// </summary>
+	/// <param name="fileName">The name of the file to delete.</param>
+	/// <param name="cancellationToken">A cancellation token to observe while waiting for the task to complete.</param>
+	/// <returns>A task that represents the asynchronous delete operation.</returns>
+	Task DeleteAsync(string fileName, CancellationToken cancellationToken = default);
+}
