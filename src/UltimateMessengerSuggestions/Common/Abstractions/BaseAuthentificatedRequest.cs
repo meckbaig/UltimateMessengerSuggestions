80bb2681e53295aa6779651eff5c9d488ba1b647
meckbaig <meckbaig@yandex.ru>
2025-07-12 22:01:38 +0500

    Add base abstraction for context injection behavior

---
+++ src/UltimateMessengerSuggestions/Common/Abstractions/BaseAuthentificatedRequest.cs
@@ -0,0 +1,35 @@
+using Meckbaig.Cqrs.Abstractons;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace UltimateMessengerSuggestions.Common.Abstractions;
+
+/// <summary>
+/// Represents a base request for operations, providing user login information and a response type.
+/// </summary>
+/// <typeparam name="TUserLoginDto">The type of the user login data transfer object. Must be a reference type.</typeparam>
+/// <typeparam name="TResponse">The type of the response returned by the query. Must inherit from <see cref="BaseResponse"/>.</typeparam>
+public abstract record BaseAuthentificatedRequest<TUserLoginDto, TResponse> : BaseRequest<TResponse>, IAuthentificatedRequest<TUserLoginDto>
+	where TResponse : BaseResponse
+	where TUserLoginDto : class
+{
+	/// <inheritdoc/>
+	[SwaggerIgnore]
+	[JsonIgnore]
+	virtual public TUserLoginDto? UserLogin { get; private set; }
+
+	/// <inheritdoc/>
+	[SwaggerIgnore]
+	[JsonIgnore]
+	[MemberNotNullWhen(true, nameof(UserLogin))]
+	virtual public bool IsAuthenticated => UserLogin != null;
+
+	/// <inheritdoc/>
+	public void SetUserLogin(TUserLoginDto userLogin)
+	{
+		if (UserLogin != null)
+			throw new InvalidOperationException("User login is already set.");
+		UserLogin = userLogin;
+	}
+}
